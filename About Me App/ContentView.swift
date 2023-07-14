4c427f1405aba3d7f8f53dc080d42cb55202e413
Donna <dondontoto28@gmail.com>
2023-07-14 07:20:32 -0700

    Initial Commit

---
+++ About Me App/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  About Me App
+//
+//  Created by scholar on 7/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var funFactText = "Tap to reveal facts"
+    var body: some View {
+        ZStack {
+            Color("lightblue")
+                .ignoresSafeArea()
+            VStack {
+                Text("Hi! my name is Donna")
+                    .font(.largeTitle)
+                
+                Image("IMG-2858")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                Text("Fun Facts About Me!")
+                    .padding()
+                Button("Tap Me") {
+                    funFactText = "My favorite book is The Seven Husbands of Evelyn Hugo, \n my favorite color is purple"
+                    
+                } .padding()
+                Text(funFactText)
+
+            }
+        }
+        
+
+        }
+        
+    
+    }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
